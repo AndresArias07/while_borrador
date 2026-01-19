90c512e09d6d3929838c81ef7bc6d2bfd53ac122
Andres Arias <s.andresarias24@gmail.com>
2026-01-19 16:57:18 +0100

    arreglo sidebar, adicion styles-sidebar para evitar problemas

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -5,6 +5,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="/style-sidebar.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
 
