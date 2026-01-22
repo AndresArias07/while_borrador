c7dbd40eec2b59a7968b3e8a15a3bc0cb43a720a
SamuelDev23 <scottsamuel2005@gmail.com>
2026-01-22 23:16:14 +0100

    fix ruta-mensajes

--- app/Views/layouts/mensajes.php
+++ app/Views/layouts/mensajes.php
@@ -3,7 +3,7 @@
 <?= $this->include("partials/head.php") ?>
 <body>
     <?= $this->include('partials/sidebar') ?>
-    <?= $this->include('partials/contentMessages') ?>
+    <?= $this->include('partials/content_messages') ?>
     <?= $this->include('partials/scripts') ?>
 </body>
 
